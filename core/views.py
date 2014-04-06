bf01a956fd6a4a68108d5ee47ca0efaa9d30784d
Matteo Scotuzzi <matteo.scotuzzi@gmail.com>
2014-04-06 08:51:48 -0700

    .

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from core.models import EntryClassification, Area
 
 
 def ohhhhh(request):
@@ -9,10 +10,13 @@ def ohhhhh(request):
 
 
 def data(request, id):
-    return render_to_response('data.html', context_instance=RequestContext(request))
+    a = Area.objects.get(id=int(id))
+    eq_set = a.entryqueue_set.filter()[:25]
+    return render_to_response('data.html', {'eq_set': eq_set}, context_instance=RequestContext(request))
 
 def index(request):
     return render_to_response('index.html', context_instance=RequestContext(request))
 
 def map(request):
-    return render_to_response('map.html', context_instance=RequestContext(request))
+    context = {'classification': EntryClassification.objects.exclude(label='neutral', entry__lat__isnull=True)}
+    return render_to_response('map.html', context, context_instance=RequestContext(request))
